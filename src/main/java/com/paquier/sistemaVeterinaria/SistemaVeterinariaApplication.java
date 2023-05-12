8eec9c4bfb3cec628d803c050055e0a2ceae975e
Victor Paquier <109697512+vicpaquier@users.noreply.github.com>
2023-05-12 16:23:01 -0400

    Add files via upload

---
+++ src/main/java/com/paquier/sistemaVeterinaria/SistemaVeterinariaApplication.java
@@ -0,0 +1,13 @@
+package com.paquier.sistemaVeterinaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaVeterinariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaVeterinariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paquier/sistemaVeterinaria/SistemaVeterinariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paquier.sistemaVeterinaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaVeterinariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
